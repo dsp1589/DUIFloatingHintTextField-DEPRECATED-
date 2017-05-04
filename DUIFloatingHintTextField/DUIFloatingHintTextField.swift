5af0f99420a9e0eeac2814e14dc55c7b8ae5177e
Dhanasekarapandian Srinivasan <dhana@dhanamacbook.local>
2017-05-04 18:12:13 -0400

    Font size issue too small and too big

--- DUIFloatingHintTextField/DUIFloatingHintTextField.swift
+++ DUIFloatingHintTextField/DUIFloatingHintTextField.swift
@@ -23,11 +23,22 @@ class DUIFloatingHintTextField : UITextField{
         }
     }
     
+    var forceHidden : Bool = false
+    
     let hintLabel = UILabel(frame: .zero)
     var hintLabelHeight : CGFloat = 0.0
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
+       setUp()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUp()
+    }
+    
+    func setUp() {
         setUpHintLabel()
         self.contentVerticalAlignment = .bottom
         self.contentHorizontalAlignment = .left
@@ -36,6 +47,10 @@ class DUIFloatingHintTextField : UITextField{
     
     func setUpHintLabel(){
         hintLabel.isOpaque = false
+//        if let f = self.font{
+//            hintLabel.font = UIFont(name: f.fontName, size: f.pointSize / 1.7)
+//        }
+        
     }
     
     override func didMoveToSuperview() {
@@ -51,52 +66,62 @@ class DUIFloatingHintTextField : UITextField{
     }
     
     func textEditing() -> Void {
-        
-        hintLabel.text = placeholder
-        hintLabel.adjustsFontSizeToFitWidth = true
-        let size = hintLabel.sizeThatFits(CGSize(width: bounds.size.width, height: hintLabelHeight))
-        if let t = text{
-            if t.isEmpty{
-                UIView.animate(withDuration: 0.5, delay: 0.0, options: .curveEaseInOut, animations: {
+        if !forceHidden{
+            hintLabel.text = placeholder
+            let size = hintLabel.sizeThatFits(CGSize(width: bounds.size.width, height: hintLabelHeight))
+            
+            if let t = text{
+                if t.isEmpty{
+                    UIView.animate(withDuration: 0.3, delay: 0.0, options: .curveEaseInOut, animations: {
+                        self.hintLabel.alpha = 0.0
+                        self.hintLabel.frame = CGRect(x: 0, y: (self.frame.size.height / 2 - size.height / 2), width: size.width, height: size.height)
+                    }, completion: { (completed) in
+                        self.hintLabel.removeFromSuperview()
+                    })
+                }else{
+                    if hintLabel.superview == nil{
+                        
+                        hintLabel.frame = CGRect(x: 0, y: (self.frame.size.height / 2 - size.height / 2), width: size.width, height: size.height)
+                        self.addSubview(hintLabel)
+                    }
+                    UIView.animate(withDuration: 0.5, delay: 0.0, options: .curveEaseInOut, animations: {
+                        self.hintLabel.alpha = 1.0
+                        self.hintLabel.frame = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+                    }, completion: { (completed) in
+                        
+                    })
+                    UIView.animate(withDuration: 0.25, animations: {
+                        
+                    })
+                    
+                }
+            }else{
+                UIView.animate(withDuration: 0.3, delay: 0.0, options: .curveEaseInOut, animations: {
                     self.hintLabel.alpha = 0.0
                     self.hintLabel.frame = CGRect(x: 0, y: (self.frame.size.height / 2 - size.height / 2), width: size.width, height: size.height)
                 }, completion: { (completed) in
                     self.hintLabel.removeFromSuperview()
                 })
-            }else{
-                if hintLabel.superview == nil{
-                    
-                    hintLabel.frame = CGRect(x: 0, y: (self.frame.size.height / 2 - size.height / 2), width: size.width, height: size.height)
-                    self.addSubview(hintLabel)
-                }
-                UIView.animate(withDuration: 0.5, delay: 0.0, options: .curveEaseInOut, animations: { 
-                    self.hintLabel.alpha = 1.0
-                    self.hintLabel.frame = CGRect(x: 0, y: 0, width: size.width, height: size.height)
-                }, completion: { (completed) in
-                    
-                })
-                UIView.animate(withDuration: 0.25, animations: {
-                   
-                })
-                
             }
-        }else{
-            UIView.animate(withDuration: 0.5, delay: 0.0, options: .curveEaseInOut, animations: {
-                self.hintLabel.alpha = 0.0
-                self.hintLabel.frame = CGRect(x: 0, y: (self.frame.size.height / 2 - size.height / 2), width: size.width, height: size.height)
-            }, completion: { (completed) in
-                self.hintLabel.removeFromSuperview()
-            })
         }
+        
     }
     
     func calcHeightForTextEditable(){
         let l = UILabel(frame: CGRect(origin: .zero, size: bounds.size))
-        l.font = self.font
+        l.font = font
         l.text = placeholder
-        hintLabelHeight =
-        bounds.height - l.sizeThatFits(CGSize(width: bounds.width, height: bounds.height)).height - 2
+        let s = l.sizeThatFits(CGSize(width: bounds.width, height: bounds.height))
         
+        let availableHeightForHint = bounds.height - s.height
+        
+        hintLabel.text = placeholder
+        hintLabel.font = UIFont(name: font!.fontName, size: (availableHeightForHint / s.height) * font!.pointSize)
+        if availableHeightForHint > s.height{
+            print("Adjust the font size of \(self.classForCoder)")
+            forceHidden = true
+        }
+        hintLabelHeight = availableHeightForHint
     }
 
 }
